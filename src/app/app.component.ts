9b04dd792985c920184f5b008db2554b5e511075
piotr tokarski <piotr.tokarski01@gmail.com>
2020-07-02 13:27:09 +0200

    New app component template and logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,22 +9,82 @@ import { TasksService } from './tasks.service';
 })
 export class AppComponent implements OnInit {
   
-  cols: number = 3;
-  activeStatus: 'oncoming'|'pending'|'completed' = 'oncoming';
-  col1: Task[];
-  col2: Task[];
-  col3: Task[];
+  cols: number = 2;
+  active: {
+    oncoming: boolean,
+    pending: boolean,
+    completed: boolean
+  };
+  groupToHide: 'oncoming'|'completed' = 'oncoming';
+  oncoming: Task[];
+  pending: Task[];
+  completed: Task[];
 
   constructor(public tasksService: TasksService){}
 
   ngOnInit(){
     //  this.tasksService.loglog(); to naprawilo błąd not a function..
-      this.tasksService.getTasks('completed', 'orderBy', 'dir', 1, 1)
-      .then(tasks=>this.disposeGroup('completed', tasks))
+
+    switch (this.cols){
+      case 1 : this.active = {
+          oncoming: true,
+          pending: false,
+          completed: false
+        };
+        break;
+      case 2 : this.active = {
+          oncoming: true,
+          pending: true,
+          completed: false
+        };
+        break;
+      default : this.active = {
+        oncoming: true,
+        pending: true,
+        completed: true
+      };
+    }
+
+    this.tasksService.getTasks('oncoming', 'orderBy', 'dir', 1, 1)
+    .then(tasks=>this.oncoming = tasks);
+
+    this.tasksService.getTasks('pending', 'orderBy', 'dir', 1, 1)
+    .then(tasks=>this.pending= tasks);
+
+    this.tasksService.getTasks('completed', 'orderBy', 'dir', 1, 1)
+    .then(tasks=>this.completed = tasks);
   }
 
+  showGroup(group: string){
+    if(this.cols===3) return;
+    if(!this.active[group])
+      switch(group){
+        case 'oncoming': 
+          if(this.cols===1)
+            this.active = {oncoming: true, pending: false, completed: false};
+          else
+            this.active = {oncoming: true, pending: false, completed: true};
+          break;
+        case 'pending':
+          if(this.cols===1)
+            this.active = {oncoming: false, pending: true, completed: false};
+          else if(this.groupToHide==='oncoming'){
+            this.active = {oncoming: false, pending: true, completed: true};
+            this.groupToHide = 'completed';
+          }else if(this.groupToHide==='completed'){
+            this.active = {oncoming: true, pending: true, completed: false};
+            this.groupToHide = 'oncoming';
+          }
+          break;
+        case 'completed': 
+          if(this.cols===1)
+            this.active = {oncoming: false, pending: false, completed: true};
+          else
+            this.active = {oncoming: true, pending: false, completed: true};
+      }
+  }
 
-  disposeGroup(status: string, tasks: Task[]): void {
+  /* disposeGroup(status: string, tasks: Task[]): void {
     switch(status){
       case 'oncoming':  this.col1 = tasks; 
                         break;
@@ -38,5 +98,5 @@ export class AppComponent implements OnInit {
                         else this.col3 = tasks;
     }
   }
-
+  */
 }
